77d90570f5e2746364f32e419d643ada46244a70
Han-Linnn <95120806+Han-Linnn@users.noreply.github.com>
2022-06-03 04:20:09 +0200

    Update test_visualization.py

--- test_visualization.py
+++ test_visualization.py
@@ -2,13 +2,12 @@
 from brane_visualization import visualization
 import unittest
 
+
 class TestStringMethods(unittest.TestCase):
     # local testing
     def test_visualization(self):
         self.assertEqual( visualization(('/data', '/data')), "Visualization is done, all the files are saved to the directory -> ./data/")
 
-    # def test_add():
-    #     assert 1+1 == 2 '1+1 == 2 is right'
 
 if __name__=="__main__":
     unittest.main()
